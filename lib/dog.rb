4a755c49d76b92af00ac84e2bd5a0c0e1f58a1cc
mbranting <mckenna.branting@gmail.com>
2018-06-07 21:39:00 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,11 @@
 # dog.rb
+class Dog
+  attr_accessor :action
+  def initialize(action)
+  @action = action
+end
+end
+bark = Dog.new("Woof!")
+sit = Dog.new("The Dog is sitting")
+puts bark.action
+puts sit.action
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,11 @@
 # person.rb
+class Person
+  attr_accessor :action
+  def initialize(action)
+    @action = action
+  end
+end
+talk = Person.new("Hello World!")
+walk = Person.new("The person is walking")
+puts talk.action
+puts walk.action
